bda55100d01d93cd38e8f214607b9a0cc20aa882
Edison_ForMan <chuanghewzh12@hotmail.com>
2020-09-02 12:02:31 +0800

    验证配置更新

--- src/api/validation/config.js
+++ src/api/validation/config.js
@@ -1,22 +1,25 @@
 /*
  * @Author: eds
  * @Date: 2020-08-31 15:45:25
- * @LastEditTime: 2020-09-02 09:11:05
+ * @LastEditTime: 2020-09-02 12:02:22
  * @LastEditors: eds
  * @Description:
  * @FilePath: \wz-city-culture-tour\src\api\validation\config.js
  */
-const signURL = "http://10.36.198.17:9000/user/getSignStr";
-// const validationURL =
-//   "https://api-szjsc.wenzhou.gov.cn/cockpit/wzdsjjsc/wzsjjsc-server/scenario/v1/validAuthorCode";
-const validationURL =
+const prodURL_HOST = "www.sourcelayer.wzcitybrain.com";
+const signURL = "https://www.sourceserver.wzcitybrain.com/api/user/getSignStr";
+const validationURL_PROD =
+  "https://api-szjsc.wenzhou.gov.cn/cockpit/wzdsjjsc/wzsjjsc-server/scenario/v1/validAuthorCode";
+const validationURL_DEV =
   "https://api-szjsc-pre.wenzhou.gov.cn/cockpit/wzdsjjsc/wzsjjsc-server/scenario/v1/validAuthorCode";
 const VERSION = "1.0";
 const APP_KEY = "kshjdnkdnkukamdjuwkmudjskskud";
 
 export default {
   signURL,
-  validationURL,
+  validationURL: window.location.href.includes(prodURL_HOST)
+    ? validationURL_PROD
+    : validationURL_DEV,
   validationJson: {
     version: VERSION,
     appKey: APP_KEY
