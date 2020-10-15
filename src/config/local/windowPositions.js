b3bf9d642b738f9eb5997e680a67a487544b6f3b
EdisonForMan <chuanghewzh12@hotmail.com>
2020-10-15 09:52:58 +0800

    电信logo

--- src/config/local/windowPositions.js
+++ src/config/local/windowPositions.js
@@ -1,17 +1,24 @@
-export default [{
-    name: "Boli_1",
-    url: "/static/gltf/window_1.gltf",
-    geometry: [120.69425553218072, 27.996902492514923, 25.4]
-}, {
-    name: "Boli_2",
-    url: "/static/gltf/window_2.gltf",
-    geometry: [120.69678173112376, 27.996857493730008, 25.39072],
-}, {
-    name: "Boli_3",
-    url: "/static/gltf/window_3.gltf",
-    geometry: [120.69905927447036, 27.998414319540105, 49.14235]
-}, {
-    name: "mildstone",
-    url: "/static/gltf/MildStone_-612820.gltf",
-    geometry: [120.6958649687849, 27.993987579868321, 36.12973]
-}]
+export default [
+    {
+        name: "Boli_1",
+        url: "/static/gltf/window_1.gltf",
+        geometry: [120.69425553218072, 27.996902492514923, 25.4]
+    }, {
+        name: "Boli_2",
+        url: "/static/gltf/window_2.gltf",
+        geometry: [120.69678173112376, 27.996857493730008, 25.39072],
+    }, {
+        name: "Boli_3",
+        url: "/static/gltf/window_3.gltf",
+        geometry: [120.69905927447036, 27.998414319540105, 49.14235]
+    }, {
+        name: "mildstone",
+        url: "/static/gltf/MildStone_-612820.gltf",
+        geometry: [120.6958649687849, 27.993987579868321, 36.12973]
+    },
+    {
+        name: "logo",
+        url: "/static/gltf/logo.gltf",
+        geometry: [120.69127270098943, 27.992010025660607, 77.08953692886914]
+    }
+]
