bccf18b7cce7e3077e0af8f5794eea718c5826a9
EdisonForMan <chuanghewzh12@hotmail.com>
2021-01-18 16:36:25 +0800

    update frames show off

--- src/components/sourcelayer/cesium_map_init.js
+++ src/components/sourcelayer/cesium_map_init.js
@@ -7,7 +7,7 @@ import window_array from "config/local/windowPositions";
  */
 export const mapConfigInit = () => {
     // window.earth.scene.globe.depthTestAgainstTerrain = false;
-    window.earth.scene.debugShowFramesPerSecond = true;
+    // window.earth.scene.debugShowFramesPerSecond = true;
     window.earth.clock.currentTime.secondsOfDay = 51830.97475229357
     // window.earth.scene.fxaa = true;
     window.earth.scene.sun.show = true;
